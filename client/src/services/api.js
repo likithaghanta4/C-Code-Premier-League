6424641f62278b39994a19e4cfe0eb64acc5931c
Likitha <likithaghanta4@gmail.com>
2026-08-08 17:01:59 +0530

    Fix API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import { TOKEN_KEY } from '../utils/constants';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
